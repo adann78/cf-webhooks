e57471f85f81bbd5569f56dacc3a7f0034a29744
adann78 <adanher78@gmail.com>
2024-12-29 11:12:32 -0700

    First commit

---
+++ src/github/github.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Headers, Body } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Post()
+  webhookHandler(      
+    @Headers('x-github-event') githubEvent: string,
+    @Body() body: any,
+  ) {
+    console.log({ githubEvent  });
+    return { githubEvent }
+  }
+}
